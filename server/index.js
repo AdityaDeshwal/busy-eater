44918f23a0104f230586e5db44fae7baeaaf8d8b
AdityaDeshwal <adityadeshwal0@gmail.com>
2023-09-08 19:34:21 +0530

    fix the pathname bug in server index.js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
-require("dotenv").config();
 const express = require('express')
+const path = require('path')
 const app = express()
-const port = process.env.PORT || 3000;
-
-const mongoDB=require("./db")
+const mongoDB= require("./db");
 mongoDB();
 
-app.use((req,res,next)=>{ 
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+app.use(express.static(path.join(__dirname, "../client/build")))
+
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-origin","http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-with,Content-Type, Accept"
   );
   next();
 })
+
 app.use(express.json())
-app.use('/api',require("./Routes/CreateUser"));
-app.use('/api',require("./Routes/DisplayData"));
-app.use('/api',require("./Routes/OrderData"));
+app.use('/api', require("./Routes/CreateUser"))
+app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
 
 app.get("*", (req, res) => {
   res.sendFile(
@@ -29,6 +30,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
