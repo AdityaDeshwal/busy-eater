2e483709fb0acd8b644696eb8022e39cf4792fa3
AdityaDeshwal <adityadeshwal0@gmail.com>
2023-09-08 19:10:39 +0530

    change port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 3000;
 
 const mongoDB=require("./db")
 mongoDB();
